558f6015f96a8f5afad4ca5b579db39b08a34666
lytmkai <lytmkai@qq.com>
2020-03-27 20:07:56 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -6,11 +6,3 @@
 	define('TPLSTYLE', '/');                        //默认模板存放的目录
 	
 	// static version file
-	define('COMMON_CSS', '/public/css/style.css?v=1571295360');      
-	define('TOUCHBOARD_CSS', '/public/touchboard/touchboard.css?v=1563410980');      
-	define('JQUERY_JS', '/public/js/jquery.js');      
-	define('UTIL_JS', '/public/js/util.js?v=1560213890');      
-	define('TOUCHBOARD_JS', '/public/touchboard/touchboard.js?v=1563410980');      
-	define('EOLOUTPUT_JS', '/public/js/eol_output.js?v=1569235746');
-	
-	define('ATTACH_URL', 'http://192.168.1.72:81/');
