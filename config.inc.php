1d032c0626f0cb1b6c2f2a12c181df271e7e2f26
lytmkai <lytmkai@qq.com>
2020-03-27 20:07:23 +0800

    Add files via upload

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php
+	define('DEBUG', 0);				      //开启调试模式 1 开启 0 关闭
+	define('CSTART', 0);                                  //缓存开关 1开启，0为关闭
+	define('CTIME', 60);                          //缓存时间
+	define('TPLPREFIX', 'html');                           //模板文件的后缀名
+	define('TPLSTYLE', '/');                        //默认模板存放的目录
+	
+	// static version file
+	define('COMMON_CSS', '/public/css/style.css?v=1571295360');      
+	define('TOUCHBOARD_CSS', '/public/touchboard/touchboard.css?v=1563410980');      
+	define('JQUERY_JS', '/public/js/jquery.js');      
+	define('UTIL_JS', '/public/js/util.js?v=1560213890');      
+	define('TOUCHBOARD_JS', '/public/touchboard/touchboard.js?v=1563410980');      
+	define('EOLOUTPUT_JS', '/public/js/eol_output.js?v=1569235746');
+	
+	define('ATTACH_URL', 'http://192.168.1.72:81/');
